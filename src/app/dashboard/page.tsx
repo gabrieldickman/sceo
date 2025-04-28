64fa3b20f59d3de2e66d1bae2fb3ab72fa2f7631
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-04-28 11:08:05 -0400

    feat: implementado info cards na pagina de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,64 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
-export default async function DashboardPage() {
-  const { userId } = await auth();
-
-  if (!userId) {
-    redirect("/login");
-  }
-
+export default function DashboardPage() {
   return (
-    <div className="flex flex-row mt-10 ml-10">
-      <h1 className="text-5xl font-bold font-white">Dashboard</h1>
+    <div className="w-full h-screen flex flex-col">
+      <header className="flex w-full p-10 sm:items-center sm:gap-5">
+        <h1 className="text-5xl font-bold font-white">Dashboard</h1>
+        <hr className="hidden sm:flex sm:flex-grow sm:border-b sm:border-[var(--gray)]" />
+      </header>
+      <main className="grid grid-cols-1 gap-5 p-5">
+        <Card className="bg-[var(--black-primary)] h-60 rounded-xl border-[var(--gray)]">
+          <CardHeader>
+            <CardTitle className="font-semibold text-white">
+              Total de Produtos
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="font-extrabold text-white text-7xl">300</h2>
+          </CardContent>
+        </Card>
+        <Card className="bg-[var(--black-primary)] h-60 rounded-xl border-[var(--gray)]">
+          <CardHeader>
+            <CardTitle className="font-semibold text-white">
+              Produtos com estoque baixo
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="font-extrabold text-white text-7xl">10</h2>
+          </CardContent>
+        </Card>
+        <Card className="bg-[var(--black-primary)] h-60 rounded-xl border-[var(--gray)]">
+          <CardHeader>
+            <CardTitle className="font-semibold text-white">
+              Produtos com estoque zerado
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="font-extrabold text-white text-7xl">5</h2>
+          </CardContent>
+        </Card>
+        <Card className="bg-[var(--green-dark)] h-60 rounded-xl border-[var(--gray)]">
+          <CardHeader>
+            <CardTitle className="font-semibold text-white">
+              Valor do inventário
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="font-extrabold text-white text-7xl">R$ 2.000</h2>
+          </CardContent>
+        </Card>
+        <Card className="bg-[var(--green-dark)] h-60 rounded-xl border-[var(--gray)]">
+          <CardHeader>
+            <CardTitle className="font-semibold text-white">
+              Valor de vendas
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="font-extrabold text-white text-7xl">R$ 450</h2>
+          </CardContent>
+        </Card>
+      </main>
     </div>
   );
 }
